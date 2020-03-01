4835cd5622896c19402440b91862972b6f27a655
JKimani77 <jkkimani77@gmail.com>
2020-03-01 19:04:51 +0300

    Created first model 'Tags'

--- flutter/models.py
+++ flutter/models.py
@@ -4,4 +4,11 @@ import datetime as dt
 
 #class for image category below
 class Tags(models.Model):
-    name = models.Charfield(max_length = 30)
+    name = models.CharField(max_length = 30)
+    def save_category(self):
+        self.save()
+    def delete_category(self):
+        self.delete()
+
+class Meta:
+        ordering =['name']
