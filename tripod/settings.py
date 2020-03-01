97daaa5de8b24c7282eab6d3fb0d15ef35a9e34e
JKimani77 <jkkimani77@gmail.com>
2020-03-01 19:33:10 +0300

    installed bootstrap3 within the virtual environment - configured the installed apps and included path in urls.py

--- tripod/settings.py
+++ tripod/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
+    'flutter.apps.FlutterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
