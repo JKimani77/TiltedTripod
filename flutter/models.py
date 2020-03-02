6d51cf51143f4b8cbbf0b8cb37129bfecb36f98a
JKimani77 <jkkimani77@gmail.com>
2020-03-02 04:34:31 +0300

    created a new migration version and new 'Image' model

--- flutter/models.py
+++ flutter/models.py
@@ -5,10 +5,19 @@ import datetime as dt
 #class for image category below
 class Tags(models.Model):
     name = models.CharField(max_length = 30)
-    def save_category(self):
+    def save_tag(self):
         self.save()
-    def delete_category(self):
+    def delete_tag(self):
         self.delete()
 
 class Meta:
         ordering =['name']
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='gallery/', blank=True)
+    category = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    # location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    
+    
