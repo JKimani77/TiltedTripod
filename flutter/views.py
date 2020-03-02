a30b102975959455a299338e7cfe76f07cb56b30
JKimani77 <jkkimani77@gmail.com>
2020-03-02 05:21:16 +0300

    created an admin superuser and fixed indentation errors in the view functions

--- flutter/views.py
+++ flutter/views.py
@@ -4,16 +4,17 @@ from .models import Tags,Image
 import datetime as dt
 
 def index(request):
-     images = Image.get_images()
+    images = Image.get_images()
     #  locations = Location.objects.all()
     return render(request,'home.html', {"images":images})
 
 def search_image(request):
     if 'image' in request.GET and request.GET['image']:
         search_word = request.GET.get('image')
-       search = Image.search_by_tag(search_word)
-       message =f"{search_word}"
-       return render(request, 'search.html',{"message":message, "images":search})
+        search = Image.search_by_tag(search_word)
+        message =f"{search_word}"
+        return render(request, 'search.html',{"message":message, "images":search})
+    
     else:
         message= "You have not searched anything yet.."
         return render(request, 'search.html', {"message":message})
