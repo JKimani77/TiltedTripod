0c371059c8a1c6d0baa3e524033e1e52a4527df0
JKimani77 <jkkimani77@gmail.com>
2020-03-02 04:39:18 +0300

    created new models and added classmethod functions

--- flutter/models.py
+++ flutter/models.py
@@ -10,6 +10,15 @@ class Tags(models.Model):
     def delete_tag(self):
         self.delete()
 
+    @classmethod
+    def get_tag_by_ID(cls, id):
+        cat = Tags.objects.get(pk=id)
+        return cat
+    
+    def update_tag(self, updated_category):
+        self.image_category = updated_category
+        self.save()
+
 class Meta:
         ordering =['name']
 
@@ -19,5 +28,21 @@ class Image(models.Model):
     image = models.ImageField(upload_to='gallery/', blank=True)
     category = models.ForeignKey(Tags, on_delete=models.CASCADE)
     # location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+
+    # @classmethod  
+    # def get_image_by_id(cls, id):
+    #     image = cls.objects.filter(id=id).all()
+    #     return image
+        
+    def update_image(self,image_name, image_description):
+        self.image_name = image_name
+        self.image_description = image_description
+        self.save()
     
     
